288a98dd6ea97425a8516bc45f6456bd7bf99daa
Jason Bentley <jbxfade@fullsail.edu>
2014-01-13 21:00:43 -0800

    passing and displaying first data to second activity

--- sportsHeadlines/gen/com/jbentley/sportsheadlines/R.java
+++ sportsHeadlines/gen/com/jbentley/sportsheadlines/R.java
@@ -24,23 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int dateMod=0x7f080007;
-        public static final int dateModHeader=0x7f080005;
-        public static final int firstButton=0x7f080001;
-        public static final int headline=0x7f080006;
-        public static final int headlineHeader=0x7f080004;
-        public static final int list=0x7f080003;
-        public static final int resultTextView=0x7f080002;
-        public static final int titleTextView=0x7f080000;
+        public static final int HeadlineTitleTextView=0x7f080000;
+        public static final int action_settings=0x7f080009;
+        public static final int dateMod=0x7f080008;
+        public static final int dateModHeader=0x7f080006;
+        public static final int firstButton=0x7f080002;
+        public static final int headline=0x7f080007;
+        public static final int headlineHeader=0x7f080005;
+        public static final int list=0x7f080004;
+        public static final int resultTextView=0x7f080003;
+        public static final int titleTextView=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_display_headlines=0x7f030000;
-        public static final int activity_headline=0x7f030001;
-        public static final int activity_headline_display=0x7f030002;
-        public static final int activity_main=0x7f030003;
-        public static final int list_header=0x7f030004;
-        public static final int list_row=0x7f030005;
+        public static final int activity_headline=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int list_header=0x7f030002;
+        public static final int list_row=0x7f030003;
     }
     public static final class menu {
         public static final int display_headlines=0x7f070000;
